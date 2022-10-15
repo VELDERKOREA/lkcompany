e0a428524a0e02a76635671cd288b97555f8cfce
ich020 <ich020@ich020ui-MacBookPro.local>
2022-10-15 14:52:07 +0900

    10.15

---
+++ public/firebase/sdk.js
@@ -0,0 +1,13 @@
+<div>
+<script defer src="/__/firebase/9.12.1/firebase-app-compat.js"></script>
+<script defer src="/__/firebase/9.12.1/firebase-auth-compat.js"></script>
+<script defer src="/__/firebase/9.12.1/firebase-database-compat.js"></script>
+<script defer src="/__/firebase/9.12.1/firebase-firestore-compat.js"></script>
+<script defer src="/__/firebase/9.12.1/firebase-functions-compat.js"></script>
+<script defer src="/__/firebase/9.12.1/firebase-messaging-compat.js"></script>
+<script defer src="/__/firebase/9.12.1/firebase-storage-compat.js"></script>
+<script defer src="/__/firebase/9.12.1/firebase-analytics-compat.js"></script>
+<script defer src="/__/firebase/9.12.1/firebase-remote-config-compat.js"></script>
+<script defer src="/__/firebase/9.12.1/firebase-performance-compat.js"></script>
+<script defer src="/__/firebase/init.js?useEmulator=true"></script>
+</div>
